18a9221fbb6db099e49418583ebaf03322dd26b7
Sohan Prabhath <soizsinghe@gmail.com>
2023-12-10 11:26:03 +0530

    basic frontend

---
+++ app/(components)/Nav.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="flex w-screen justify-between items-center">
+      <div className="flex text-xl mx-2">hello</div>
+      <div className="flex">
+        <Link href="/" className="nbtn">
+          Home
+        </Link>
+        <Link href="/ticket" className="nbtn">
+          New Ticket
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TicketCard from "./(components)/TicketCard";
+
+const page = () => {
+  return (
+    <div className="p-3 w-screen">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
+  );
+};
+
+export default page;
